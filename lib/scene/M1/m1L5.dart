802f736cc8bd78f319e858c026c8fcf7fd665101
ABHISHEK THORAT <abhithorat.24@gmail.com>
2024-05-20 03:16:26 +0530

    module1Level5_Selction level

--- lib/scene/M1/m1L5.dart
+++ lib/scene/M1/m1L5.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -9,92 +7,11 @@ class M1L5 extends StatefulWidget {
 }
 
 class _M1L5State extends State<M1L5> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  // int M1L5Attempts = 0;
-  // int M1L5Point = 0;
-
-  Future<void> updateFirebaseUserAnswer(String savedSequence) async {
-    try {
-      String userUid = await getCurrentUserUid();
-      if (userUid.isNotEmpty) {
-        FirebaseFirestore firestore = FirebaseFirestore.instance;
-        DocumentReference userDocRef =
-            firestore.collection('users').doc(userUid);
-        DocumentReference scoreDocRef =
-            userDocRef.collection('score').doc('M1');
-
-        // Get the current user's answer map
-        DocumentSnapshot<Map<String, dynamic>> snapshot =
-            await scoreDocRef.get() as DocumentSnapshot<Map<String, dynamic>>;
-        Map<String, dynamic> userAnswerMap =
-            snapshot.data()?['userAnswer'] ?? {};
-
-        // Create or update L5 and store the saved sequence
-        userAnswerMap['L5'] = {'savedSequence': savedSequence};
-
-        // Update the 'userAnswer' field in the 'Score-M1' document with the modified map
-        await scoreDocRef.update({
-          'userAnswer': userAnswerMap,
-        });
-      }
-    } catch (e) {
-      print('Error updating user answer in Firestore: $e');
-    }
-  }
-
-  Future<String> getCurrentUserUid() async {
-    try {
-      User? user = _auth.currentUser;
-      if (user != null) {
-        return user.uid;
-      }
-    } catch (e) {
-      print('Error getting user UID: $e');
-    }
-    return ''; // Return an empty string if the user is not authenticated
-  }
-
-  // void updateFirebaseDataM1L5() async {
-  //   try {
-  //     FirebaseFirestore firestore = FirebaseFirestore.instance;
-  //     String userUid = await getCurrentUserUid();
-
-  //     if (userUid.isNotEmpty) {
-  //       // Reference to the user's document
-  //       DocumentReference userDocRef =
-  //           firestore.collection('users').doc(userUid);
-
-  //       // Reference to the 'score' document with document ID 'M1'
-  //       DocumentReference scoreDocRef =
-  //           userDocRef.collection('score').doc('M1');
-
-  //       DocumentReference attemptDocRef =
-  //           userDocRef.collection('attempt').doc('M1');
-
-  //       // Update the fields in the 'score' document
-  //       await scoreDocRef.update({
-  //         'M1L5Point': M1L5Point,
-  //       });
-  //       await attemptDocRef.update({
-  //         'M1L5Attempts': M1L5Attempts,
-  //       });
-  //     }
-  //   } catch (e) {
-  //     print('Error updating data: $e');
-  //   }
-  // }
-
-  List<String> tasks = [
+  List<String> selectedTasks = [];
+  List<String> allTasks = [
     'Fruit eated',
     'Helped Grandchild with Season',
     'Helped Grandchild with Date',
-    'Picking Up Grandchild'
-  ];
-  String savedSequence = '';
-  final List<String> correctSequence = [
-    'Helped Grandchild with Date',
-    'Helped Grandchild with Season',
-    'Fruit eated',
     'Picking Up Grandchild'
   ];
 
@@ -176,7 +93,32 @@ class _M1L5State extends State<M1L5> {
     );
   }
 
-  void showCelebrationDialog() {
+  void onSavePressed() {
+    setState(() {
+      // Compare selectedTasks with correct sequence
+      if (_checkSequence()) {
+        _showCorrectDialog();
+      } else {
+        _showIncorrectDialog();
+      }
+    });
+  }
+
+  bool _checkSequence() {
+    List<String> correctSequence = [
+      'Helped Grandchild with Date',
+      'Helped Grandchild with Season',
+      
+      'Fruit eated',
+      'Picking Up Grandchild'
+    ];
+    return selectedTasks.length == correctSequence.length &&
+        List.generate(selectedTasks.length,
+            (index) => selectedTasks[index] == correctSequence[index])
+            .every((element) => element);
+  }
+
+  void _showCorrectDialog() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -186,9 +128,6 @@ class _M1L5State extends State<M1L5> {
           actions: [
             TextButton(
               onPressed: () {
-                // M1L5Attempts++;
-                // M1L5Point = 1;
-                // updateFirebaseDataM1L5();
                 Navigator.pop(context);
               },
               child: Text('OK'),
@@ -199,13 +138,13 @@ class _M1L5State extends State<M1L5> {
     );
   }
 
-  void showTryAgainDialog() {
+  void _showIncorrectDialog() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Oops!'),
-          content: Text('Please try again.'),
+          content: Text('Your answer is incorrect. Please try again.'),
           actions: [
             TextButton(
               onPressed: () {
@@ -219,107 +158,131 @@ class _M1L5State extends State<M1L5> {
     );
   }
 
+  void moveToAllTasks(String task) {
+    setState(() {
+      selectedTasks.remove(task);
+      allTasks.add(task);
+    });
+  }
+
+  void moveToSelectedTasks(String task) {
+    setState(() {
+      selectedTasks.add(task);
+      allTasks.remove(task);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Task Done Sequence'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: tasks
-                .map(
-                  (task) => Draggable<String>(
-                    data: task,
-                    childWhenDragging: Container(
-                      width: 150,
-                      height: 70,
-                      color: Colors.grey[300],
-                      child: Center(
-                        child: Text(
-                          task,
-                          style: TextStyle(fontSize: 16, color: Colors.black),
-                        ),
-                      ),
-                    ),
-                    feedback: Material(
-                      elevation: 4.0,
-                      child: Container(
-                        width: 150,
-                        height: 70,
-                        color: Colors.blue,
-                        child: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Selected Tasks:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Wrap(
+                spacing: 10.0,
+                runSpacing: 10.0,
+                children: selectedTasks
+                    .map(
+                      (task) => GestureDetector(
+                        onTap: () => moveToAllTasks(task),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 12.0,
+                            horizontal: 16.0,
+                          ),
+                          margin: EdgeInsets.all(4.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(30.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 4,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
                           child: Text(
                             task,
-                            style: TextStyle(fontSize: 16, color: Colors.white),
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    child: DragTarget<String>(
-                      builder: (context, candidateData, rejectedData) {
-                        return Card(
-                          elevation:
-                              6.0, // Increased elevation for a stronger shadow
-                          shadowColor: Colors.grey[600], // Shadow color
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                10.0), // Rounded corners for the card
+                    )
+                    .toList(),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'All Tasks:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Wrap(
+                spacing: 10.0,
+                runSpacing: 10.0,
+                children: allTasks
+                    .map(
+                      (task) => GestureDetector(
+                        onTap: () => moveToSelectedTasks(task),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 12.0,
+                            horizontal: 16.0,
                           ),
-                          child: Container(
-                            width: 150,
-                            height: 70,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(
-                                  10.0), // Rounded corners for the container
-                            ),
-                            child: Center(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 8.0),
-                                child: Text(
-                                  task,
-                                  style: TextStyle(
-                                      fontSize: 16, color: Colors.black),
-                                  textAlign: TextAlign.center,
-                                ),
+                          margin: EdgeInsets.all(4.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(30.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 4,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
                               ),
+                            ],
+                          ),
+                          child: Text(
+                            task,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        );
-                      },
-                      onAccept: (data) {
-                        setState(() {
-                          int index = tasks.indexOf(data);
-                          tasks[tasks.indexOf(task)] = data;
-                          tasks[index] = task;
-                        });
-                      },
-                    ),
-                  ),
-                )
-                .toList(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ],
           ),
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            savedSequence = tasks.join(', ');
-          });
-
-          // Compare savedSequence with correctSequence
-          if (savedSequence == correctSequence.join(', ')) {
-            updateFirebaseUserAnswer(savedSequence);
-            showCelebrationDialog();
-          } else {
-            showTryAgainDialog();
-          }
-        },
+        onPressed: onSavePressed,
         child: Text('Save'),
       ),
     );
